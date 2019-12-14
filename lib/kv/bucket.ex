87090488526efc4e6bbaff8c23616c0ab93b0f5b
Oscar Bernardini <bernardini687@gmail.com>
2019-12-14 15:38:39 +0100

    reformat named function keys_to_values

--- lib/kv/bucket.ex
+++ lib/kv/bucket.ex
@@ -12,7 +12,10 @@ defmodule KV.Bucket do
   Gets the value/s of the `key/s` from the `bucket`.
   """
   def get(bucket, keys) when is_list(keys) do
-    keys_to_values = fn map -> Enum.map(keys, &Map.get(map, &1)) end
+    keys_to_values = fn map ->
+      Enum.map(keys, &Map.get(map, &1))
+    end
+
     Agent.get(bucket, &keys_to_values.(&1))
   end
 
