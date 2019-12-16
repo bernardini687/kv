056216ffd969c51921417de09dd41aec4b145371
Oscar Bernardini <bernardini687@gmail.com>
2019-12-16 15:44:52 +0100

    start supervisor

--- lib/kv.ex
+++ lib/kv.ex
@@ -1,5 +1,8 @@
 defmodule KV do
-  @moduledoc """
-  Documentation for KV.
-  """
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    KV.Supervisor.start_link(name: KV.Supervisor)
+  end
 end
